23c30987d19862833a26d9d7399d91ebbec4409c
Sumit Subhash Patil <sumitgatpatil41@gmail.com>
2026-01-11 23:59:34 +0530

    retrive

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxValueValidator
 
 # Create your models here.
 
 class Book(models.Model):
     Title = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinLengthValidator(1), MaxValueValidator(5)])
+    author = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.Title} ({self.rating})"
